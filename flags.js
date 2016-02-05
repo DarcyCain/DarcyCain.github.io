41d9d1cc4f2fbf95306826d08507f73fe88d0197
DarcyCain <Dcain2017@kellogg.northwestern.edu>
2016-02-05 11:46:06 -0600

    added java

--- flags.js
+++ flags.js
@@ -1,10 +1,13 @@
 // This code runs when the page loads
 $(function() {
-
+	var flagcounter =0;
   $(".thumbnail").on("click", function(event) {
     event.preventDefault();
     var elementThatWasClicked = $(this);
+    flagcounter++;
     console.log(elementThatWasClicked);
+    console.log(flagcounter);
+    $(".flagcounter").text(flagcounter);
     elementThatWasClicked.parent().remove();
   })
 
